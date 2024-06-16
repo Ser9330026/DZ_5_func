ca6566ea63630dcce127ca97dedfabb6d1e63f4d
SergeyB <ser9330026@yandex.ru>
2024-06-16 13:38:54 +0500

    Добывили две функции нахождения мфксимального элемента в двумерном массиве

--- DZ_5_func/DZ_5_func.cpp
+++ DZ_5_func/DZ_5_func.cpp
@@ -115,6 +115,39 @@ void fooMax(double arr[], int length)
 }
 
 
+//Нахождения максимального значения в двумерном массиве
+void fooMaxDoubleArr(int arr[][5], int rows, int cols)
+{
+    int max = 0;
+    max = arr[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (max < arr[i][j]) {
+                max = arr[i][j];
+            }
+        }
+        
+    }
+    std::cout << "Максимальное число в массиве: " << max << std::endl;
+}
+
+void fooMaxDoubleArr(double arr[][5], int rows, int cols)
+{
+    double max = 0;
+    max = arr[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (max < arr[i][j]) {
+                max = arr[i][j];
+            }
+        }
+
+    }
+    std::cout << "Максимальное число в массиве: " << max << std::endl;
+}
+
+
+
 int main()
 {
     setlocale(LC_ALL, "rus");
@@ -152,6 +185,31 @@ int main()
 
     double arr1[SIZE]{ 2.66, 4.55, 3.66, 7.77, 3.44, 6.88, 8.99, 2.09, 222.444, 2.999 };
     fooMax(arr1, SIZE);
+
+    //Нахождения максимального значения в двумерном массиве
+    {
+        const int ROWS = 5;
+        const int COLS = 5;
+        int arr[ROWS][COLS] = { {1, 4, 5, 2, 6},
+            {11, 14, 15, 12, 16},
+            {12, 42, 52, 22, 62},
+            {13, 43, 54, 21, 63},
+            {10, 40, 50, 20, 60} };
+        fooMaxDoubleArr(arr, ROWS, COLS);
+        std::cout << std::endl;
+    }
+
+    {
+        const int ROWS = 5;
+        const int COLS = 5;
+        double arr[ROWS][COLS] = { {1.33, 4.44, 5.22, 2.33, 6.44},
+            {11.2, 14.2, 15.2, 12.2, 16.2},
+            {12.3, 42.4, 52.888, 22.444, 62.1},
+            {1.3, 4.3, 5.4, 2.1, 6.3},
+            {10.333, 4.0, 50.333, 20.444, 60.666} };
+        fooMaxDoubleArr(arr, ROWS, COLS);
+        std::cout << std::endl;
+    }
     
 
 
